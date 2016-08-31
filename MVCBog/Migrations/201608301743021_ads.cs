832311ca933c18f182715a6999668e3d6afd2a31
Martin78910 <martin4o78@gmail.com>
2016-08-31 12:30:18 +0300

    Промени страници Регистрация Вход

---
+++ MVCBog/Migrations/201608301743021_ads.cs
@@ -0,0 +1,16 @@
+namespace MVCBog.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ads : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
